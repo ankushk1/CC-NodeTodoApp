10799af7024b1f911806e6ba3dde936289e1d9a3
Ankush <ankushkapoor179@gmail.com>
2021-10-10 14:01:48 +0530

    added cors

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use('/user', userRoutes);
 app.use('/', todoRoutes);
 app.set('secretkey', process.env.SECRET_KEY);
